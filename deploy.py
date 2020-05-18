cfd5402d5ae39be5dae49f4a2daae302edf78bcd
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-05-18 17:12:00 +0300

    Restful API

--- deploy.py
+++ deploy.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Deployed!</h1>'
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'about': "Hello World!"}
+
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}, 201
+
+
+class Multi(Resource):
+    def get(self, num):
+        return {'result': num * 10}
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Multi, '/multi/<int:num>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
