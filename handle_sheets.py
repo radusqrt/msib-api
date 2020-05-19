be5d4cb9d4ab0735c0dea4f10627724268168200
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-05-19 14:33:35 +0300

    Try catch for everything

--- handle_sheets.py
+++ handle_sheets.py
@@ -2,25 +2,28 @@ import pandas as pd
 
 
 def handle_df(df):
-    df = df[df['Iti place'] == 'DA']
-
-    dfs = {}
-    dfs['angio'] = df[(df['Antiangiogeneza'] == 'DA')
-                      | (df['Angiogeneza'] == 'DA')]
-    dfs['regenerare'] = df[df['Regenerare'] == 'DA']
-    dfs['microbiom'] = df[df['Microbiom'] == 'DA']
-    dfs['adn'] = df[df['ADN'] == 'DA']
-    dfs['imunitate'] = df[df['Imunitate'] == 'DA']
-
     try:
-        sampled_dfs = {key: dfs[key].sample(3) for key in dfs}
+        df = df[df['Iti place'] == 'DA']
+
+        dfs = {}
+        dfs['angio'] = df[(df['Antiangiogeneza'] == 'DA')
+                          | (df['Angiogeneza'] == 'DA')]
+        dfs['regenerare'] = df[df['Regenerare'] == 'DA']
+        dfs['microbiom'] = df[df['Microbiom'] == 'DA']
+        dfs['adn'] = df[df['ADN'] == 'DA']
+        dfs['imunitate'] = df[df['Imunitate'] == 'DA']
+
+        try:
+            sampled_dfs = {key: dfs[key].sample(3) for key in dfs}
+        except:
+            return "E nevoie de cel puțin trei alimente din fiecare tip", 400
+
+        response = {key: [sampled_dfs[key].iloc[idx].to_dict()
+                          for idx in range(sampled_dfs[key].shape[0])] for key in sampled_dfs}
+
+        return response, 200
     except:
-        return "E nevoie de cel puțin trei alimente din fiecare tip", 400
-
-    response = {key: [sampled_dfs[key].iloc[idx].to_dict()
-                      for idx in range(sampled_dfs[key].shape[0])] for key in sampled_dfs}
-
-    return response, 200
+        return "Eroare necunoscută", 204
 
 
 def handle_xlsx(filename):
